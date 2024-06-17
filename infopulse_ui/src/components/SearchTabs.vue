9567026e9761ee7a3a447a89dde7d0c34f3ac230
Joshua Bonner <joshua.bonner@outlook.com>
2024-06-17 15:29:59 -0400

    IPSS-18: Article Search Tabs created

--- infopulse_ui/src/components/SearchTabs.vue
+++ infopulse_ui/src/components/SearchTabs.vue
@@ -1,22 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { useSearchStore } from '@/stores/search'
+
+const tab: Ref<string | null> = ref(null)
+const searchStore = useSearchStore()
+</script>
 <template>
-  <v-dialog>
-    <v-list>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Item 1</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Item 2</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Item 3</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-dialog>
+  <v-tabs v-model="tab" direction="vertical">
+    <v-tab v-for="search in searchStore.searches" :key="search.id" :value="search.query">
+      {{ search.query }}
+    </v-tab>
+  </v-tabs>
+
+  <v-tabs-window v-model="tab" class="tab-content">
+    <v-tabs-window-item
+      v-for="search in searchStore.searches"
+      :key="search.id"
+      :value="search.content"
+    >
+      <v-card flat>
+        <v-card-text> Content for {{ search.content }} </v-card-text>
+      </v-card>
+    </v-tabs-window-item>
+  </v-tabs-window>
 </template>
+
+<style scoped>
+.tab-content {
+  margin-top: 10px;
+  width: 90%;
+  padding: 0;
+}
+</style>
