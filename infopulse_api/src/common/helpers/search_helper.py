455a9f28ec00a7a0517b2ddb4162145d87f5c5b7
Joshua Bonner <joshua.bonner@outlook.com>
2024-07-18 16:25:09 -0400

    IPSS-34: Implemented Advanced Search Logic

--- infopulse_api/src/common/helpers/search_helper.py
+++ infopulse_api/src/common/helpers/search_helper.py
@@ -2,6 +2,10 @@ import hashlib
 
 import requests
 from bs4 import BeautifulSoup
+from common.data_handlers.user_handler import UserHandler
+from models.user import UserSearchPref
+
+user_handler = UserHandler()
 
 
 def url_to_unique_int(url: str) -> int:
@@ -15,3 +19,14 @@ def get_article_text(url: str) -> str:
     soup = BeautifulSoup(response.content, "html.parser")
     paragraphs = soup.find_all("p")
     return " ".join([p.text for p in paragraphs[1:-1]])
+
+
+def construct_query(search_query: str):
+    search_pref: UserSearchPref = user_handler.get_user_search_prefs("test")
+    return {
+        "q": search_query,
+        "from_param": search_pref.search_from.strftime("%Y-%m-%d"),
+        "to": search_pref.search_to.strftime("%Y-%m-%d"),
+        "language": "en",
+        "sort_by": search_pref.sort_by,
+    }
